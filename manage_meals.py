32700e18e021842e2328593745c12ae668e6d421
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2024-12-06 14:04:04 -0500

    finished creation of manageMenu window

--- manage_meals.py
+++ manage_meals.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-from tkinter import ttk
 
 class Manage_meals(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Manage Meals')
-        self.geometry('300x800')    
+        self.geometry('300x800'),
+        self.resizable(False, False)
 
         sampleMeals = (
             'meatballs',
@@ -28,16 +28,32 @@ class Manage_meals(tk.Tk):
             text= 'Meal List:'
         ).grid(row= 0, column= 0, sticky= 'w')
 
+        self.selectedMeal = tk.IntVar()
+
         self.mealList = tk.Listbox(
             self,
             width= '30',
             height= '10',
+            selectmode= 'browse'
         )
 
         for meal in sampleMeals:
-            self.mealList.insert(0, meal)
+            self.mealList.insert(tk.END, meal)
 
         self.mealList.grid(row= 1, column= 0, columnspan= 3, sticky= 'we', padx= 15, pady= 3)
+
+        self.deleteMealBtn = tk.Button(
+            self,
+            text= 'Delete Meal',
+            fg= 'red',
+            command= self.deleteMeal
+        ).grid(row= 2, column= 0, columnspan= 3)
+
+    def deleteMeal(self):
+        selection = self.mealList.curselection()
+
+        if selection:
+            self.mealList.delete(selection[0])
 
 def runWindow():
     Manage_meals().mainloop()
