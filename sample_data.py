95ea5a4c72b56292b90368b599940cbec8b06175
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2024-12-07 14:12:42 -0500

    created sample data

---
+++ sample_data.py
@@ -0,0 +1,27 @@
+
+
+sample_meals = (
+            'meatballs',
+            'sandwhich',
+            'pasta',
+            'spaghet',
+            'bananas',
+            'oatmeal',
+            'tacos',
+            'porcupine soup',
+            'silly rabbits',
+            'cereal',
+            'footballs',
+            'christmas trees',
+            'stuff I can not pronounce'
+        )
+
+sample_ingredients = {
+            'pepperoni': 'pcs',
+            'ham': 'lbs',
+            'sausage': 'pcs',
+            'tomato': 'lbs',
+            'mustard': 'oz',
+            'black olives': 'pc',
+            'cheese': 'pcs'
+        }
