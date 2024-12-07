1e403932dfd51201b6d42860f074bf6babaf5707
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2024-12-07 14:24:54 -0500

    refactored to group grid methods together

--- manage_meals.py
+++ manage_meals.py
@@ -14,8 +14,8 @@ class Manage_meals(tk.Tk):
         self.meal_list_label = tk.Label(
             self,
             text= 'Meal List:'
-        ).grid(row= 0, column= 0, sticky= 'w')
-
+        )
+        
         self.selected_meal = tk.IntVar()
 
         self.meal_list = tk.Listbox(
@@ -28,21 +28,26 @@ class Manage_meals(tk.Tk):
         for meal in self.meals:
             self.meal_list.insert(tk.END, meal)
 
-        self.meal_list.grid(row= 1, column= 0, sticky= 'we', padx= 15, pady= 3)
 
         self.delete_meal_btn = tk.Button(
             self,
             text= 'Delete Meal',
             fg= 'red',
             command= self.delete_meal
-        ).grid(row= 2, column= 0)
+        )
+        
 
         self.add_meal_btn = tk.Button(
             self,
             text= 'Add New Meal',
             fg= 'green',
             command= Add_new_meal
-        ).grid(row= 3, column= 0)
+        )
+        
+        self.meal_list_label.grid(row= 0, column= 0, sticky= 'w')
+        self.meal_list.grid(row= 1, column= 0, sticky= 'we', padx= 15, pady= 3)
+        self.delete_meal_btn.grid(row= 2, column= 0)
+        self.add_meal_btn.grid(row= 3, column= 0)
 
     def delete_meal(self):
         selection = self.meal_list.curselection()
