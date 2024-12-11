f1ef3256267fe2406fe7266bfb5dc51cfcdd5a0b
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2024-12-10 19:17:39 -0500

    added logic for add ingredient

--- manage_ingredients.py
+++ manage_ingredients.py
@@ -1,65 +1,120 @@
 import tkinter as tk
-from add_new_meal import *
 from sample_data import *
 
 class Manage_ingredients(tk.Toplevel):
-    '''
-    Window that allows the user to manage meals stored in memory for the application. Only meals previously added will be shown. The user can then delete meals out of the list or add a new meal by using a different window
-    '''
+ 
+
     def __init__(self):
         super().__init__()
         self.title('Manage Ingredients')
-        self.geometry('300x300'),
+        self.geometry('400x450'),
         self.resizable(False, False)
 
-        # Currently this pulls from a sample meals file. This will eventually pull from a pickled data source. 
-        # Todo: update meals to pull from sample meals
-        self.ingredients = sample_ingredients
+        self.ingredients = []
+
+        for key, value in sample_ingredients.items():
+            self.ingredients.append(f'{key}, measured in {value}')
 
         self.ingredient_list_label = tk.Label(
             self,
             text= 'Ingredient List:'
         )
 
-        # listbox will display all of the meals currently in memory
         self.ingredient_list = tk.Listbox(
             self,
             width= '30',
             height= '10',
         )
 
-        for meal in self.ingredients:
-            self.ingredient_list.insert(tk.END, meal)
+        for ingredient in self.ingredients:
+            self.ingredient_list.insert(tk.END, ingredient)
 
-        # button to delete any meals out
+        
         self.delete_ingredient_btn = tk.Button(
             self,
             text= 'Delete Ingredient',
             fg= 'red',
             command= self.delete_ingredient
         )
-        
-        # button that takes the user to the window to create a new meal
-        self.add_ingredient_btn = tk.Button(
+
+        self.add_ingredient_section_label = tk.Label(
             self,
-            text= 'Add New Ingredient',
+            text= 'Add New Ingredient:'
+        )
+
+        self.add_ingredient_frame = tk.LabelFrame(
+            self,
+            width= 200,
+            height= 200
+        )
+
+        self.add_ingreient_name_label = tk.Label(
+            self.add_ingredient_frame,
+            text= 'New Ingredient Name: '
+        )
+
+        self.add_ingredient_name_input = tk.Entry(
+            self.add_ingredient_frame
+        )
+
+        self.add_ingredient_quantifier_label = tk.Label(
+            self.add_ingredient_frame,
+            text= 'New Ingredient Quantifier:'
+        )
+
+        self.add_ingredients_quanitfier_input = tk.Entry(
+            self.add_ingredient_frame,
+            width= 5
+        )
+
+        self.quantifier_examples = tk.Label(
+            self.add_ingredient_frame,
+            text= '(pcs, oz, lbs, ect)'
+        )
+
+        self.error_message_var = tk.StringVar(value= '')
+        
+        self.error_message_area = tk.Label(
+            self.add_ingredient_frame,
+            textvariable= self.error_message_var,
+            fg= 'red'
+        )
+    
+        self.add_ingredient_btn = tk.Button(
+            self.add_ingredient_frame,
+            text= 'Add New Ingredient To Ingredient List',
             fg= 'green',
-            command= Add_new_meal
+            command= self.add_ingredient
         )
         
         self.ingredient_list_label.grid(row= 0, column= 0, sticky= 'w')
         self.ingredient_list.grid(row= 1, column= 0, sticky= 'we', padx= 15, pady= 3)
         self.delete_ingredient_btn.grid(row= 2, column= 0)
-        self.add_ingredient_btn.grid(row= 3, column= 0)
+        self.add_ingredient_section_label.grid(row= 3, column= 0, padx= 10, sticky= 'w')
+        
+        self.add_ingredient_frame.grid(row= 4, column= 0, padx= 10, pady= 10, sticky= 'w')
+        self.add_ingreient_name_label.grid(row= 0, column= 0, pady= 10, sticky= 'w')
+        self.add_ingredient_name_input.grid(row= 0, column= 1, columnspan= 3, padx= 10, pady= 10, sticky= 'w')
+        self.add_ingredient_quantifier_label.grid(row= 1, column= 0, pady= 10, sticky= 'w')
+        self.add_ingredients_quanitfier_input.grid(row= 1, column= 1, pady= 10, padx= 10, sticky= 'w')
+        self.quantifier_examples.grid(row= 1, column= 3, pady= 10, sticky= 'w')
+        self.error_message_area.grid(row= 2, columnspan= 4, padx= 10, pady= 10, sticky= 'we')
+        self.add_ingredient_btn.grid(row= 3, columnspan= 4, pady= 10, sticky= 'we')
+
 
     def delete_ingredient(self):
-        '''
-        This method pulls a list of seleted items and deletes them from the meal list, which in turn should then remove them from memory
-        '''
-        # curseselection returns an array of selected items
+        
         selection = self.ingredient_list.curselection()
 
-        # if there is a selection, delete the first one in the selection array. 
-        # todo, change this so that the user can select any number of meals and delete them
         if selection:
             self.ingredient_list.delete(selection[0])
+
+    def add_ingredient(self):
+        ingredient_name = self.add_ingredient_name_input.get()
+        ingredient_quantifier = self.add_ingredients_quanitfier_input.get()
+
+        if not ingredient_name and not ingredient_quantifier:
+            self.error_message_var.set('Ingredient Name and quanitfier must both be filled out!')
+        else:
+            self.ingredient_list.insert(tk.END, f'{ingredient_name}, measured in {ingredient_quantifier}')
+        
