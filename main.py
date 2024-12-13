bffb7da33a4149572b749029b8a72c5e23e08743
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2024-12-13 17:46:02 -0500

    had to spread out days to be able to grab them, wrote export method

--- main.py
+++ main.py
@@ -15,24 +15,30 @@ class Main_window(tk.Tk):
         self.resizable(False, False)
         self.title('Main Window')
 
-        # Initialize days of the week
-        self.days = (
-            'Monday', 
-            'Tuesday', 
-            'Wednesday', 
-            'Thursday', 
-            'Friday', 
-            'Saturday', 
-            'Sunday'
-            )
         
         self.week_frame = tk.Frame(
             self
         )
 
-        # Go through days of the week, create frames
-        for row, day in enumerate(self.days):
-            self.make_day_frame(day, row, self.week_frame)
+        with open('meals.dat', 'rb') as f:
+            try:
+                self.meals: list[Meal] = pickle.load(f)
+            except EOFError:
+                self.meals = []
+
+        self.meal_list = []
+
+        for meal in self.meals:
+            self.meal_list.append(meal.get_name())
+
+
+        with open('ingredients.dat', 'rb') as f:
+            try:
+                self.ingredients: list[Ingredient] = pickle.load(f)
+            except EOFError:
+                self.ingredients = []
+       
+        self.make_day_frames(self.week_frame)
 
         btn_height = 2
         btn_width = 12
@@ -61,7 +67,8 @@ class Main_window(tk.Tk):
             self,
             text= 'Export Menu and List',
             height= btn_height,
-            width= btn_width
+            width= btn_width,
+            command= self.create_menu_list
         )
         
         self.week_frame.grid(row= 0, column= 0, rowspan= 3)
@@ -69,46 +76,168 @@ class Main_window(tk.Tk):
         self.manage_ingredients_btn.grid(row= 1, column= 7)
         self.export_btn.grid(row= 2, column= 7)
 
-    def make_day_frame(self, day: str, column, week_frame: tk.Frame):
+
+        
+
+    def make_day_frames(self, week_frame: tk.Frame):
         '''
         Generates a frame for a single day for the week calendar
         '''
 
-        self.day_frame = tk.LabelFrame(
+        self.monday_frame = tk.LabelFrame(
             week_frame,
             bg= '#434343',
             width= 166,
             height= 190,
         )
-        
 
-        self.day_label = tk.Label(
+        self.monday_label = tk.Label(
             week_frame,
-            text= day,
+            text= 'Monday',
             bg= '#434343'
         )
-        
-        with open('meals.dat', 'rb') as f:
-            try:
-                self.meals: list[Meal] = pickle.load(f)
-            except EOFError:
-                self.meals = []
 
-        self.meal_list = []
+        self.monday_meal_selector = ttk.Combobox(
+            self.monday_frame,
+            width= 10,
+            values= self.meal_list,
+        )
 
-        for meal in self.meals:
-            self.meal_list.append(meal.get_name())
+        self.tuesday_frame = tk.LabelFrame(
+            week_frame,
+            bg= '#434343',
+            width= 166,
+            height= 190,
+        )
 
-        self.meal_selector = ttk.Combobox(
-            self.day_frame,
+        self.tuesday_label = tk.Label(
+            week_frame,
+            text= 'Tuesday',
+            bg= '#434343'
+        )
+
+        self.tuesday_meal_selector = ttk.Combobox(
+            self.tuesday_frame,
             width= 10,
             values= self.meal_list
         )
 
+        self.wednesday_frame = tk.LabelFrame(
+            week_frame,
+            bg= '#434343',
+            width= 166,
+            height= 190,
+        )
+
+        self.wednesday_label = tk.Label(
+            week_frame,
+            text= 'Wednesday',
+            bg= '#434343'
+        )
+
+        self.wednesday_meal_selector = ttk.Combobox(
+            self.wednesday_frame,
+            width= 10,
+            values= self.meal_list
+        )
+
+        self.thursday_frame = tk.LabelFrame(
+            week_frame,
+            bg= '#434343',
+            width= 166,
+            height= 190,
+        )
+
+        self.thursday_label = tk.Label(
+            week_frame,
+            text= 'Thursday',
+            bg= '#434343'
+        )
+
+        self.thursday_meal_selector = ttk.Combobox(
+            self.thursday_frame,
+            width= 10,
+            values= self.meal_list
+        )
+
+        self.friday_frame = tk.LabelFrame(
+            week_frame,
+            bg= '#434343',
+            width= 166,
+            height= 190,
+        )
+
+        self.friday_label = tk.Label(
+            week_frame,
+            text= 'Friday',
+            bg= '#434343'
+        )
+
+        self.friday_meal_selector = ttk.Combobox(
+            self.friday_frame,
+            width= 10,
+            values= self.meal_list
+        )
+
+        self.saturday_frame = tk.LabelFrame(
+            week_frame,
+            bg= '#434343',
+            width= 166,
+            height= 190,
+        )
+
+        self.saturday_label = tk.Label(
+            week_frame,
+            text= 'Saturday',
+            bg= '#434343'
+        )
+
+        self.saturday_meal_selector = ttk.Combobox(
+            self.saturday_frame,
+            width= 10,
+            values= self.meal_list
+        )
+
+        self.sunday_frame = tk.LabelFrame(
+            week_frame,
+            bg= '#434343',
+            width= 166,
+            height= 190,
+        )
+
+        self.sunday_label = tk.Label(
+            week_frame,
+            text= 'Sunday',
+            bg= '#434343'
+        )
+
+        self.sunday_meal_selector = ttk.Combobox(
+            self.sunday_frame,
+            width= 10,
+            values= self.meal_list
+        )
         
-        self.day_label.grid(row= 0, column= column)
-        self.meal_selector.grid(row= 1, column= column, padx= 21,pady= 62)
-        self.day_frame.grid(row= 0, column= column, rowspan= 3, pady= 10)
+        self.monday_label.grid(row= 0, column= 0)
+        self.monday_meal_selector.grid(row= 1, column= 0, padx= 21,pady= 62)
+        self.monday_frame.grid(row= 0, column= 0, rowspan= 3, pady= 10)
+        self.tuesday_label.grid(row= 0, column= 1)
+        self.tuesday_meal_selector.grid(row= 1, column= 1, padx= 21,pady= 62)
+        self.tuesday_frame.grid(row= 0, column= 1, rowspan= 3, pady= 10)
+        self.wednesday_label.grid(row= 0, column= 2)
+        self.wednesday_meal_selector.grid(row= 1, column= 2, padx= 21,pady= 62)
+        self.wednesday_frame.grid(row= 0, column= 2, rowspan= 3, pady= 10)
+        self.thursday_label.grid(row= 0, column= 3)
+        self.thursday_meal_selector.grid(row= 1, column= 3, padx= 21,pady= 62)
+        self.thursday_frame.grid(row= 0, column= 3, rowspan= 3, pady= 10)
+        self.friday_label.grid(row= 0, column= 4)
+        self.friday_meal_selector.grid(row= 1, column= 4, padx= 21,pady= 62)
+        self.friday_frame.grid(row= 0, column= 4, rowspan= 3, pady= 10)
+        self.saturday_label.grid(row= 0, column= 5)
+        self.saturday_meal_selector.grid(row= 1, column= 5, padx= 21,pady= 62)
+        self.saturday_frame.grid(row= 0, column= 5, rowspan= 3, pady= 10)
+        self.sunday_label.grid(row= 0, column= 6)
+        self.sunday_meal_selector.grid(row= 1, column= 6, padx= 21,pady= 62)
+        self.sunday_frame.grid(row= 0, column= 6, rowspan= 3, pady= 10)
 
     def remake_week(self):
         self.week_frame.destroy()
@@ -118,9 +247,84 @@ class Main_window(tk.Tk):
 
         self.new_week_frame.grid(row= 0, column= 0, rowspan= 3)
 
-        for row, day in enumerate(self.days):
-            self.make_day_frame(day, row, self.new_week_frame)
+        
+        self.make_day_frames(self.new_week_frame)
 
+    def create_menu_list(self):
+        monday_meal = self.monday_meal_selector.get()
+        tuesday_meal = self.tuesday_meal_selector.get()
+        wednesday_meal = self.wednesday_meal_selector.get()
+        thursday_meal = self.thursday_meal_selector.get()
+        friday_meal = self.friday_meal_selector.get()
+        saturday_meal = self.saturday_meal_selector.get()
+        sunday_meal = self.sunday_meal_selector.get()
+
+        meals_for_the_week = [
+            monday_meal, 
+            tuesday_meal, 
+            wednesday_meal, 
+            thursday_meal, 
+            friday_meal, 
+            saturday_meal, 
+            sunday_meal
+            ]
+        
+        ingredient_list = []
+        meal_list: list[Meal] = []
+
+        for meal_name in meals_for_the_week:
+            for meal in self.meals:
+                if meal.get_name() == meal_name:
+                    meal_list.append(meal)
+
+        for ingredient in self.ingredients:
+            quantity = 0
+            ingredient_name = ''
+            quantifier = 'qty'
+            for meal in meal_list:
+                ingredient_name = ingredient.get_name()
+                meal_ingredients = meal.get_ingredient_names()
+                if ingredient_name not in meal_ingredients:
+                    continue
+                quantity += meal.get_specific_ingredient_quantity(ingredient_name)
+                quantifier = meal.get_specific_ingredient_quantifier(ingredient_name)
+            ingredient_list.append(f'{quantity:.2f} {quantifier} of {ingredient_name}')
+        
+                
+        
+
+        #quantity = meal.get_specific_ingredient_quantity(meal_name)
+         #           if quantity == 0:
+           #             continue
+          #          ingredient_list.append(f'{quantity} {meal.get_specific_ingredient_quantifier(meal_name)} of {ingredient.get_name()}')
+
+
+
+        with open('menu.txt', 'w') as f:
+            i = 0
+
+            days = (
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+                'Sunday'
+            )
+
+            f.write('Menu:\n----------------\n')
+            for meal_name in meals_for_the_week:
+                f.write(f'{days[i]}: {meal_name} \n')
+                i += 1
+            f.write(f'\nIngredients Needed for Meals selected:\n-----------------\n')
+            for ingredient in ingredient_list:
+                f.write(ingredient + '\n')
+        
+        messagebox.showinfo(
+            title= 'Sucess!',
+            message= 'Menu list sucessfully exported to file, see menu.txt for list'
+        )
 
 
 if __name__ == '__main__':
