64a891393cebe3e22d2e785f49552f7a583456f2
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2024-12-12 20:01:22 -0500

    pulling meals from meals file, resaving and having main window redraw days on close

--- manage_meals.py
+++ manage_meals.py
@@ -1,20 +1,30 @@
 import tkinter as tk
 from add_new_meal import *
 from sample_data import *
+from classes import *
+from main import *
+import pickle
+
 
 class Manage_meals(tk.Toplevel):
     '''
     Window that allows the user to manage meals stored in memory for the application. Only meals previously added will be shown. The user can then delete meals out of the list or add a new meal by using a different window
     '''
-    def __init__(self):
+    def __init__(self, root: Main_window):
         super().__init__()
+        self.root = root
         self.title('Manage Meals')
         self.geometry('300x300'),
         self.resizable(False, False)
+        self.protocol("WM_DELETE_WINDOW", lambda: self.destroy() or self.update_main_window())
 
         # Currently this pulls from a sample meals file. This will eventually pull from a pickled data source. 
-        # Todo: update meals to pull from sample meals
-        self.meals = sample_meals
+        
+        with open('meals.dat', 'rb') as f:
+            try:
+                self.meals: list[Meal] = pickle.load(f)
+            except EOFError:
+                self.meals = []
 
         self.meal_list_label = tk.Label(
             self,
@@ -29,7 +39,7 @@ class Manage_meals(tk.Toplevel):
         )
 
         for meal in self.meals:
-            self.meal_list.insert(tk.END, meal)
+            self.meal_list.insert(tk.END, meal.get_name())
 
         # button to delete any meals out
         self.delete_meal_btn = tk.Button(
@@ -63,3 +73,12 @@ class Manage_meals(tk.Toplevel):
         # todo, change this so that the user can select any number of meals and delete them
         if selection:
             self.meal_list.delete(selection[0])
+            self.meals.pop(selection[0])
+
+            with open('meals.dat', 'wb') as f:
+                pickle.dump(self.meals, f)
+
+    def update_main_window(self):
+        self.root.remake_week()
+        self.root.deiconify()
+            
