6fdb9e08d37a856681aeaf4ec8cb57ece94a4651
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2024-12-12 19:58:58 -0500

    made a frame for the week, made a method that redraws the days

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import tkinter as tk
 from tkinter import ttk
 from manage_meals import *
 from manage_ingredients import *
-
+from classes import *
+import pickle
 class Main_window(tk.Tk):
     '''
     Class that generates the main menu for the menu app
@@ -15,7 +16,7 @@ class Main_window(tk.Tk):
         self.title('Main Window')
 
         # Initialize days of the week
-        days = (
+        self.days = (
             'Monday', 
             'Tuesday', 
             'Wednesday', 
@@ -25,9 +26,13 @@ class Main_window(tk.Tk):
             'Sunday'
             )
         
+        self.week_frame = tk.Frame(
+            self
+        )
+
         # Go through days of the week, create frames
-        for row, day in enumerate(days):
-            self.make_day_frame(day, row)
+        for row, day in enumerate(self.days):
+            self.make_day_frame(day, row, self.week_frame)
 
         btn_height = 2
         btn_width = 12
@@ -39,7 +44,7 @@ class Main_window(tk.Tk):
             height= btn_height,
             width= btn_width,
             activebackground= '#FF0000',
-            command= Manage_meals,
+            command= lambda: Manage_meals(self)
         )
         
 
@@ -59,18 +64,18 @@ class Main_window(tk.Tk):
             width= btn_width
         )
         
+        self.week_frame.grid(row= 0, column= 0, rowspan= 3)
         self.manage_meals_btn.grid(row= 0, column= 7, padx= 10)
         self.manage_ingredients_btn.grid(row= 1, column= 7)
         self.export_btn.grid(row= 2, column= 7)
 
-
-    def make_day_frame(self, day: str, column):
+    def make_day_frame(self, day: str, column, week_frame: tk.Frame):
         '''
         Generates a frame for a single day for the week calendar
         '''
 
         self.day_frame = tk.LabelFrame(
-            self,
+            week_frame,
             bg= '#434343',
             width= 166,
             height= 190,
@@ -78,23 +83,39 @@ class Main_window(tk.Tk):
         
 
         self.day_label = tk.Label(
-            self.day_frame,
+            week_frame,
             text= day,
             bg= '#434343'
         )
         
-
-        self.meals = sample_meals
+        with open('meals.dat', 'rb') as f:
+            try:
+                self.meals: list[Meal] = pickle.load(f)
+            except EOFError:
+                self.meals = []
 
         self.meal_selector = ttk.Combobox(
             self.day_frame,
             width= 10,
-            values= self.meals
         )
+
+        for meal in self.meals:
+            self.meal_selector.insert(tk.END, meal.get_name())
         
         self.day_label.grid(row= 0, column= column)
         self.meal_selector.grid(row= 1, column= column, padx= 21,pady= 62)
         self.day_frame.grid(row= 0, column= column, rowspan= 3, pady= 10)
+
+    def remake_week(self):
+        self.week_frame.destroy()
+        self.new_week_frame = tk.Frame(
+            self
+        )
+
+        self.new_week_frame.grid(row= 0, column= 0, rowspan= 3)
+
+        for row, day in enumerate(self.days):
+            self.make_day_frame(day, row, self.new_week_frame)
 
 
 
